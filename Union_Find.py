36b80ed6891870fabda9a879e8a2bdc1cdc2e17e
djagmlrhks3 <djagmlrhks3@naver.com>
2021-03-03 23:58:20 +0900

    0303(수) | Algorithm_Notes | Union_Find

---
+++ Union_Find.py
@@ -0,0 +1,51 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+cycle = False # Check cycle
+
+for i in range(e):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b): # Detect cycle
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print("Yes, cycle")
+else:
+    print("No, cycle")
+
+# Input
+"""
+7 9
+1 2
+1 5
+2 3
+2 6
+3 4
+4 6
+4 7
+5 6
+6 7
+"""
+# Input
+"""
+Yes, cycle
+"""
